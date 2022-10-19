cde560332988420b7a0acb0e6b4819474d609bd0
Arif Ramadhan <ariframadhan0859@gmail.com>
2022-10-19 15:31:05 +0800

    Ketiga

--- src/components/about/AboutMe.vue
+++ src/components/about/AboutMe.vue
@@ -9,7 +9,7 @@ export default {
             },
             {
                id: 2,
-               bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",
+               bio: "About Arif",
             },
          ],
       };
